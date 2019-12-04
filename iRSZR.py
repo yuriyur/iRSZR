0af3252dd8aa5d45c5e17b2cff95479e36b72784
Yuri <33545766+yuriyur@users.noreply.github.com>
2019-12-04 13:16:46 +0400

    Update iRSZR.py

--- iRSZR.py
+++ iRSZR.py
@@ -1,3 +1,12 @@
+from datetime import datetime, date, time
 import os
 from PIL import Image
 import PySimpleGUI as sg
+
+layout = [  [sg.Text('Выберите папку:', size=(20, 1)), sg.InputText(), sg.FolderBrowse('...')],
+            [sg.Text('Размер изображений по максимальной стороне в px:', size=(40, 1)), sg.Input(1600, size=(10, 1))], 
+            [sg.Text('Прогресс'), sg.ProgressBar(1, orientation='h', size=(44, 20), key='progress')],
+            [sg.Button('Ок', size=(10, 1)), sg.Cancel('Exit')]
+         ]
+
+window = sg.Window('iRSZR - изменение размера изображений пакетно', layout)
